cab347d1cb292f3b4739be7e04a76681c80f1af4
Kirill Mekenya <31892167+KirillMekenya@users.noreply.github.com>
2020-05-05 14:20:01 +0300

    add overloaded method

--- task_2/Firm.java
+++ task_2/Firm.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Firm {
 
-    private Employee [] employees = new Employee[8];
+    private Employee[] employees = new Employee[8];
     private int allSalary;
 
     public Employee[] getEmployees() {
@@ -13,17 +13,26 @@ public class Firm {
         this.employees = employees;
     }
 
-    public void addEmployee (Employee employee, int index)
-    {
+    public void addEmployee(Employee employee) {
+
+        for (int i = 0; i < this.employees.length; i++) {
+            if (employees[i] == null) {
+                this.employees[i] = employee;
+                break;
+            }
+        }
+    }
+
+
+    public void addEmployee(Employee employee, int index) {
 
         this.employees[index] = employee;
     }
 
-    public int calculateSalary ()
-    {
-        for (Employee employee: this.employees
-             ) {
-            this.allSalary+=employee.getSalary();
+    public int calculateSalary() {
+        for (Employee employee : this.employees
+        ) {
+            this.allSalary += employee.getSalary();
         }
         return this.allSalary;
 
